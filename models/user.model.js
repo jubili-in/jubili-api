0ed0d17101e65cd469618d4c6abf17a6e2b3b2f9
subhankar <subhankarg2524@gmail.com>
2024-10-28 12:23:37 +0530

    auth refined user

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     fullname: String,
-    email: String,
+    email: {
+        type: String,
+        unique: true,
+    },
     password: String,
     phone: {
         type: String,
