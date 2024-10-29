a6afa3ece11b2c0c50018b7fe823435fb3af6d34
subhankar <subhankarg2524@gmail.com>
2024-10-29 16:41:40 +0530

    cors whitelist for render, local:3000, local IPs

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require('cors');
 
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoute")
 const vendorRoutes = require("./routes/vendorRoute")
+
+// Whitelist configuration for CORS
+const allowedOrigins = [
+    "https://kickstart-59ea.onrender.com",
+    "http://localhost:3000",              // Localhost for development
+    /^http:\/\/192\.168\.\d{1,3}\.\d{1,3}(:\d+)?$/ // Local network IPs
+];
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -23,4 +44,4 @@ app.get("/", (req, res)=>{
 
 app.listen(process.env.PORT || 8000, ()=>{
     console.log(`server is running on port ${process.env.PORT}`);
-})
+}) 
