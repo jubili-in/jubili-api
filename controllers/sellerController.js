d355ffe0077384f17e9b10a13b8ba19aa5271db8
SoumikGhosh111 <soumikbuie2001@gmail.com>
2025-06-17 10:23:26 +0530

    changed the sameSite settings to allow cross origin req

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -25,8 +25,8 @@ const signupSeller = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       // secure: process.env.NODE_ENV === 'production', // only on HTTPS in production
-      sameSite: 'Strict', // need to be changed to strict when ready for production
-      // sameSite: 'none',  // making the sameSite to none for accesing while in developement 
+      sameSite: 'None', // need the none setting to send res to different origins
+      
       secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
